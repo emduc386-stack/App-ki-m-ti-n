5f16c39532ee003d0236bffb53be0e5b1c9c687d
emduc386-stack <emduc386@gmail.com>
2026-07-29 23:42:12 +0700

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,10 @@
+const apps = [
+  {
+    name: "Link4m",
+    image: "images/link4m.png",
+    page: "pages/link4m.html",
+    rating: 5,
+    tag: "HOT",
+    description: "Rút tiền bằng MB Bank."
+  }
+];
