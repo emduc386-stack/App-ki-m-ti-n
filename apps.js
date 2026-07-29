12407ef74f9a0e641658cfb0e3a77cdd0fadd346
emduc386-stack <emduc386@gmail.com>
2026-07-30 00:15:01 +0700

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,10 +1,10 @@
-const apps = [
-  {
-    name: "Link4m",
-    image: "images/link4m.png",
-    page: "pages/link4m.html",
-    rating: 5,
-    tag: "HOT",
-    description: "Rút tiền bằng MB Bank."
-  }
-];
+{
+  name: "Link4m",
+  image: "images/link4m.png",
+  page: "pages/link4m.html",
+  website: "https://link4m.com",
+  referral: "https://link4m.com/register?ref=abc123",
+  rating: 5,
+  tag: "HOT",
+  description: "Kiếm tiền bằng rút gọn liên kết."
+}
