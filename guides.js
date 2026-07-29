6346ef2539185812e953b0857537a6b10a39e153
emduc386-stack <emduc386@gmail.com>
2026-07-30 00:54:58 +0700

    Create guides.js

---
+++ guides.js
@@ -0,0 +1,20 @@
+const guides = [
+  {
+    title: "Cách kiếm tiền với Link4m",
+    description: "Hướng dẫn đăng ký, tạo link và rút tiền.",
+    content: `
+<h2>Cách kiếm tiền với Link4m</h2>
+
+<p>Đây là nơi bạn viết toàn bộ hướng dẫn.</p>
+
+<p>Bạn có thể thêm:</p>
+
+<ul>
+<li>Cách đăng ký</li>
+<li>Cách tạo link</li>
+<li>Cách chia sẻ</li>
+<li>Cách rút tiền</li>
+</ul>
+`
+  }
+];
